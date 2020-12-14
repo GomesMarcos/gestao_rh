58793aad76c386107493e3102d6af419498728a5
GomesMarcos <gomes.marcosjf@gmail.com>
2020-12-13 22:12:12 -0300

    Criando a App Empresas

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Empresa(models.Model):
-    mome = models.CharField(max_length=100, help_text="Nome da Empresa")
+  nome = models.CharField(max_length=100, help_text="Nome da empresa")
