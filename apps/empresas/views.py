1f0e8eae34b16c0e43cb322a6ef320200d7b4696
GomesMarcos <gomes.marcosjf@gmail.com>
2020-12-18 21:42:56 -0300

    update empresa model,,model

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Empresa
+
+class EmpresaCreate(CreateView):
+  model = Empresa
+  fields = ['nome',]
+
+  def form_valid(self, form):
+    obj = form.save()
+
+    # Atribuindo empresa criada ao funcionário logado
+    funcionario = self.request.user.funcionario
+    funcionario.empresa = obj
+    funcionario.save()
+
+    return HttpResponse('OK')
+
+class EmpresaEdit(UpdateView):
+  model = Empresa
+  fields = ['nome',]
