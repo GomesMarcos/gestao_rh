53934554b2813728ab90fee8310cf5a3d74aee65
GomesMarcos <gomes.marcosjf@gmail.com>
2020-12-19 23:04:23 -0300

    WIP funcionarios CRUD

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import FuncionariosList, FuncionariosEdit, FuncionariosDelete
 
 urlpatterns = [
-    path('', Home),
+    path('', FuncionariosList.as_view(), name="list_funcionarios"),
+    path('editar/<int:pk>', FuncionariosEdit.as_view(), name="update_funcionario"),
+    path('deletar/<int:pk>', FuncionariosDelete.as_view(), name="delete_funcionario"),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-def Home(request):
-  return HttpResponse('Olá')
+from .models import Funcionario
+
+class FuncionariosList(ListView):
+  model = Funcionario
+
+  def get_queryset(self):
+    empresa_logada = self.request.user.funcionario.empresa
+    return self.model.objects.filter(empresa=empresa_logada)
+
+class FuncionariosEdit(UpdateView):
+  model = Funcionario
+  fields = ['nome', 'departamentos']
+  
+class FuncionariosDelete(DeleteView):
+  model = Funcionario
+  success_url = reverse_lazy('list_funcionarios')
