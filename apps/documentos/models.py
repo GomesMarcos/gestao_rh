11a8213da9b0c9e57930d714346c73e9bea349b4
GomesMarcos <gomes.marcosjf@gmail.com>
2020-12-21 23:21:26 -0300

    update documentos model

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -4,6 +4,7 @@ from apps.funcionarios.models import Funcionario
 class Documento(models.Model):
   titulo = models.CharField(max_length=100)
   pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+  arquivo = models.FileField(upload_to='documentos')
 
 
   def __str__(self):
