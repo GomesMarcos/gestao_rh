bba90495f0f60f5cac302757d4678afa0a9c1777
GomesMarcos <gomes.marcosjf@gmail.com>
2020-12-24 01:39:19 -0300

    update static files routes on settings

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -89,8 +89,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "staticfiles",
 ]
+
+STATIC_ROOT = BASE_DIR / "static"
 
 LOGIN_REDIRECT_URL = 'home'
 
